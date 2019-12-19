b8becc8373bd17580a0df172ac01c9c3b1d15a28
hs <helpfulscripts@gmail.com>
2019-12-18 20:57:25 -0800

    updated sharedConfigs

--- sharedGruntConfig.js
+++ sharedGruntConfig.js
@@ -61,7 +61,8 @@ module.exports = (grunt, dir, dependencies, type, lib) => {
     grunt.registerTask('build-base',    ['clean:dist', 'clean:docs', 'build-html', 'build-css', 'copy:bin', 'copy:example']);
     grunt.registerTask('buildMin',      (type === 'node')?['build-base', 'build-js', 'doc', 'test', 'run-coveralls'] : 
                                                           ['build-base', 'build-js', 'webpack:appDev', 'webpack:appProd', 'doc', 'test', 'run-coveralls']);
-    grunt.registerTask('buildDev',      ['build-base', 'build-js', 'webpack:appDev']);
+    grunt.registerTask('buildDev',      (type === 'node')?['build-base', 'build-js'] : 
+                                                          ['build-base', 'build-js', 'webpack:appDev']);
 
     //------ Entry-point MultiTasks
     grunt.registerTask('default',       ['product']);	
